51091a27e5ca0931ed3fa07cdb05e05b3d7cf106
Your Name <you@example.com>
2023-08-30 01:03:05 -0400

    second commit

---
+++ admin-web/upload_script/popup.php
@@ -0,0 +1,26 @@
+<?php
+// Check if the request comes from the main HTML page
+$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+
+// Replace this with the actual URL of your main HTML page
+$validReferer = 'http://192.168.72.128/admin-web/upload_script/index.html';
+
+if ($referer !== $validReferer) {
+    echo "Access denied. Please start the script from the main page.";
+    exit;
+}
+
+$cmd = 'bash ./test.sh';
+
+while (@ob_end_flush()); // end all output buffers if any
+
+$proc = popen($cmd, 'r');
+echo '<pre>';
+while (!feof($proc))
+{
+    echo fread($proc, 4096);
+    @flush();
+}
+echo '</pre>';
+?>
+
---
+++ admin-web/upload_script/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+date=`date`
+# Rest of your script
+echo "Line 1"
+echo "Line 2"
+echo "Line 3"
+
+echo "executed on $date"
+for ((i = 1; i <= 10; i++)); do
+    echo $i
+    sleep 2s
+done
+
+exit
+
